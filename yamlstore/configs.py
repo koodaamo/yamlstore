e23263567eba00548b94f592e2763ce6dc6fb0c9
Petri Savolainen <petri.savolainen@koodaamo.fi>
2023-12-03 23:11:28 +0200

    allow config title & description setting, cleanup

--- yamlstore/configs.py
+++ yamlstore/configs.py
@@ -8,21 +8,13 @@ class Configuration(Document):
     "read only document"
 
     def __setitem__(self, key, value):
-        raise PermissionError("Config is read-only")
+        if key in ("title", "description"):
+            super().__setitem__(key, value)
+        else:
+            raise PermissionError("Config is read-only")
 
 
 class ConfigurationDatabase(DocumentDatabase):
     "configuration database with added root config"
 
     ITEM = Configuration
-
-    def __init__(self, source:Union[TextIOWrapper, str, None]=None):
-
-        if isinstance(source, TextIOWrapper):
-            self.data = dict(self.ITEM(source))
-        else:
-            super().__init__(source)
-
-    def __iadd__(self, filename):
-        "add a new configuration document to the database root"
-        self._root = self.ITEM(Path(filename))
